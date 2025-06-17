ee6a021664f02afbc4d32b2c5e91f2759c774656
Mình Phuoc Le <radensparks2507@gmail.com>
2025-06-17 15:52:22 +0700

    Payment Test Inclusion

--- src/services/payment/payment.controller.ts
+++ src/services/payment/payment.controller.ts
@@ -1,14 +1,22 @@
 import { Controller, Post, Param, Body, Req, UseGuards } from '@nestjs/common';
+import { ApiBody } from '@nestjs/swagger';
 import { PaymentsService } from './payment.service';
 import { PaymentMethod } from './payment.types';
-import { JwtAuthGuard } from '../../auth/jwt-auth.guard'; 
+import { JwtAuthGuard } from '../../auth/jwt-auth.guard';
 
 @Controller('payments')
-@UseGuards(JwtAuthGuard) 
+@UseGuards(JwtAuthGuard)
 export class PaymentsController {
   constructor(private readonly paymentsService: PaymentsService) {}
 
   @Post('pay/:orderId')
+  @ApiBody({
+    schema: {
+      example: {
+        method: 'paypal' // or 'on_delivery'
+      }
+    }
+  })
   async pay(
     @Param('orderId') orderId: number,
     @Body('method') method: PaymentMethod,
@@ -20,6 +28,11 @@ export class PaymentsController {
   }
 
   @Post('refund/:orderId')
+  @ApiBody({
+    schema: {
+      example: {}
+    }
+  })
   async refundOrder(@Param('orderId') orderId: number, @Req() req) {
     const userId = req.user?.id;
     const userRole = req.user?.role;
@@ -27,6 +40,11 @@ export class PaymentsController {
   }
 
   @Post('cancel/:orderId')
+  @ApiBody({
+    schema: {
+      example: {}
+    }
+  })
   async cancelOrder(@Param('orderId') orderId: number, @Req() req) {
     const userId = req.user?.id;
     const userRole = req.user?.role;
@@ -34,6 +52,11 @@ export class PaymentsController {
   }
 
   @Post('paypal/capture/:orderId')
+  @ApiBody({
+    schema: {
+      example: {}
+    }
+  })
   async capturePaypal(@Param('orderId') orderId: number, @Req() req) {
     const userId = req.user?.id;
     const userRole = req.user?.role;
