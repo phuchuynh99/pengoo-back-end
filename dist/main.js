b928cf043c9c7cfc3e6be7ffb26c5985988655c3
Mình Phuoc Le <radensparks2507@gmail.com>
2025-07-22 01:51:09 +0700

    Supabase Minor Swagger Config

--- dist/main.js
+++ dist/main.js
@@ -7,44 +7,36 @@ const swagger_1 = require("@nestjs/swagger");
 const jwt_auth_guard_1 = require("./auth/jwt-auth.guard");
 const core_2 = require("@nestjs/core");
 let cachedServer;
-async function bootstrap() {
-    const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    const reflector = app.get(core_2.Reflector);
-    app.useGlobalGuards(new jwt_auth_guard_1.JwtAuthGuard(reflector));
-    app.enableCors({
-        origin: [
-            'http://localhost:3000',
-            'http://localhost:3001',
-            'http://localhost:4000',
-        ],
-        credentials: true,
-    });
-    app.setGlobalPrefix('api');
-    const config = new swagger_1.DocumentBuilder()
-        .setTitle('Swagger API')
-        .setDescription('UI for API testing')
-        .setVersion('1.0')
-        .addTag('Playmaker')
-        .addBearerAuth({
-        type: 'http',
-        scheme: 'bearer',
-        bearerFormat: 'JWT',
-    }, 'jwt')
-        .build();
-    const documentFactory = () => swagger_1.SwaggerModule.createDocument(app, config);
-    swagger_1.SwaggerModule.setup('swagger-api', app, documentFactory);
-    await app.listen(process.env.PORT ?? 3000);
-    console.log("-------------------------------------------");
-    console.log("---| http://localhost:3000/swagger-api |---");
-    console.log("-------------------------------------------");
-}
 async function handler(req, res) {
     if (!cachedServer) {
         const app = await core_1.NestFactory.create(app_module_1.AppModule, { bodyParser: false });
+        const reflector = app.get(core_2.Reflector);
+        app.useGlobalGuards(new jwt_auth_guard_1.JwtAuthGuard(reflector));
+        app.enableCors({
+            origin: [
+                'http://localhost:3000',
+                'http://localhost:3001',
+                'http://localhost:4000',
+            ],
+            credentials: true,
+        });
+        app.setGlobalPrefix('api');
+        const config = new swagger_1.DocumentBuilder()
+            .setTitle('Swagger API')
+            .setDescription('UI for API testing')
+            .setVersion('1.0')
+            .addTag('Playmaker')
+            .addBearerAuth({
+            type: 'http',
+            scheme: 'bearer',
+            bearerFormat: 'JWT',
+        }, 'jwt')
+            .build();
+        const documentFactory = () => swagger_1.SwaggerModule.createDocument(app, config);
+        swagger_1.SwaggerModule.setup('swagger-api', app, documentFactory);
         await app.init();
         cachedServer = app.getHttpServer();
     }
     cachedServer.emit('request', req, res);
 }
-bootstrap();
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -7,54 +7,42 @@ import { Server } from 'http';
 
 let cachedServer: Server;
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const reflector = app.get(Reflector);
-  app.useGlobalGuards(new JwtAuthGuard(reflector));
-
-  app.enableCors({
-    origin: [
-      'http://localhost:3000', // <-- Add this line for Swagger UI
-      'http://localhost:3001', // main site
-      'http://localhost:4000', // admin dashboard
-    ],
-    credentials: true,
-  });
-
-  app.setGlobalPrefix('api'); // <--- Add this line
-
-  const config = new DocumentBuilder()
-    .setTitle('Swagger API')
-    .setDescription('UI for API testing')
-    .setVersion('1.0')
-    .addTag('Playmaker')
-    .addBearerAuth(
-      {
-        type: 'http',
-        scheme: 'bearer',
-        bearerFormat: 'JWT',
-      },
-      'jwt', // this name is used later in @ApiBearerAuth('jwt')
-    )
-    .build();
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('swagger-api', app, documentFactory);
-
-  await app.listen(process.env.PORT ?? 3000);
-
-  console.log("-------------------------------------------");
-  console.log("---| http://localhost:3000/swagger-api |---")
-  console.log("-------------------------------------------");
-
-}
-
 export default async function handler(req, res) {
   if (!cachedServer) {
     const app = await NestFactory.create(AppModule, { bodyParser: false });
+    const reflector = app.get(Reflector);
+    app.useGlobalGuards(new JwtAuthGuard(reflector));
+
+    app.enableCors({
+      origin: [
+        'http://localhost:3000',
+        'http://localhost:3001',
+        'http://localhost:4000',
+      ],
+      credentials: true,
+    });
+
+    app.setGlobalPrefix('api');
+
+    const config = new DocumentBuilder()
+      .setTitle('Swagger API')
+      .setDescription('UI for API testing')
+      .setVersion('1.0')
+      .addTag('Playmaker')
+      .addBearerAuth(
+        {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        },
+        'jwt',
+      )
+      .build();
+    const documentFactory = () => SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('swagger-api', app, documentFactory);
+
     await app.init();
     cachedServer = app.getHttpServer();
   }
   cachedServer.emit('request', req, res);
 }
-
-bootstrap();
